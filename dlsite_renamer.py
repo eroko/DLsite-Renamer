58e9d709b5efe3753d348826d4e0a047c0fb8258
ch010060 <ch010060@gmail.com>
2020-09-14 23:30:33 +0800

    Refactor and support comic and game works 1. Support BJ(Comic) and VJ(game) works renaming 2. Add config for BJ and VJ and default config 3. Add new attribute "author" of work

--- dlsite_renamer.py
+++ dlsite_renamer.py
@@ -10,15 +10,21 @@ import os
 import json
 
 # 默认配置
-template = 'workno [circle] title (cv)'  # 默认命名模板
+template = 'workno title '  # 默认命名模板
+template_RJ = 'workno title (cv) '  # 默认RJ命名模板(Voice)
+template_BJ = 'workno [circle (author)] [title] '  # 默认BJ命名模板(Comic)
+template_VJ = 'workno [title] '  # 默认VJ命名模板(Game)
+
 replace_rules = []  # 替换规则
 
 RJ_WEBPATH = 'https://www.dlsite.com/maniax/work/=/product_id/'
+BJ_WEBPATH = 'https://www.dlsite.com/books/work/=/product_id/'
+VJ_WEBPATH = 'https://www.dlsite.com/pro/work/=/product_id/'
 R_COOKIE = {'adultchecked': '1'}
 
 # re.compile()返回一个匹配对象
 # ensure path name is exactly RJ###### or RT######
-pattern = re.compile("^R[EJT]\d{6}$")
+pattern = re.compile("[BRV][EJ]\d{6}")
 # filter to substitute illegal filenanme characters to " "
 filter = re.compile('[\\\/:"*?<>|]+')
 
@@ -56,25 +62,30 @@ def find_all(source, dest):
         dest_list.remove(x)
     return dest_list
 
-# 从文件夹名称中提取r_code
+# 从文件夹名称中提取code
 
 
-def get_r_code(originalName, matchCode):
+def get_code(originalName, matchCode):
     index_list = find_all(originalName, matchCode)
     if index_list == -1:
         return ""
     for i in range(0, len(index_list)):
         r_idx = index_list[i]
-        r_code = originalName[r_idx:(r_idx)+8]
+        code = originalName[r_idx:(r_idx)+8]
         pattern = re.compile("^"+matchCode+"\d{6}$")
-        if pattern.match(r_code):
-            return r_code.upper()
+        if pattern.match(code):
+            return code.upper()
     return ""
 
 
-def match_rj(rj_code):
+def match_code(work_code):
     # requests库是一个常用于http请求的模块
-    url = RJ_WEBPATH + rj_code
+    if work_code[0] == "R" or work_code[0] == "r":
+        url = RJ_WEBPATH + work_code
+    if work_code[0] == "B" or work_code[0] == "b":
+        url = BJ_WEBPATH + work_code
+    if work_code[0] == "V" or work_code[0] == "v":
+        url = VJ_WEBPATH + work_code
     try:
         # allow_redirects=False 禁止重定向
         r = s.get(url, allow_redirects=False, cookies=R_COOKIE)
@@ -91,23 +102,10 @@ def match_rj(rj_code):
             '//span[@itemprop="brand" and @class="maker_name"]/*/text()')[0]
         cvList = tree.xpath(
             '//*[@id="work_outline"]/tr/th[contains(text(), "声優")]/../td/a/text()')
-        return 200, title, circle, cvList
+        authorList = tree.xpath(
+            '//*[@id="work_maker"]/tr/th[contains(text(), "著者")]/../td/a/text()')
 
-    except os.error as err:
-        text.insert(tk.END, "**请求超时!\n")
-        text.insert(tk.END, "  请检查网络连接\n")
-        return "", "", "", []
-    url = RT_WEBPATH + rt_code
-    try:
-        r = s.get(url + '.html', allow_redirects=False, cookies=R_COOKIE)
-        if r.status_code != 200:
-            #print("    Status code:", r.status_code, "\nurl:", url)
-            return r.status_code, "", "", []
-
-        tree = html.fromstring(r.content)
-        title = tree.xpath('//div[@class="works_summary"]/h3/text()')[0]
-        circle = tree.xpath('//a[@class="summary_author"]/text()')[0]
-        return 200, title, circle, []
+        return 200, title, circle, cvList, authorList
 
     except os.error as err:
         text.insert(tk.END, "**请求超时!\n")
@@ -131,43 +129,50 @@ def nameChange():
             if os.path.isdir(os.path.join(path, file)):
                 # 获取文件夹原始名称
                 originalName = file
-                # 尝试获取r_code
-                r_code = ""
-                for matchCode in ['RJ', 'rj', 'RT', 'rt']:
-                    r_code = get_r_code(originalName, matchCode)
-                    if r_code:
+                # 尝试获取code
+                code = ""
+                for matchCode in ['RJ', 'rj', 'BJ', 'bj', 'VJ', 'vj']:
+                    code = get_code(originalName, matchCode)
+                    if code:
                         break
-                # 如果没能提取到r_code
-                if r_code == "":
+                # 如果没能提取到code
+                if code == "":
                     continue  # 跳过该文件夹
                 else:
-                    #print('Processing: ' + r_code)
-                    text.insert(tk.END, 'Processing: ' + r_code + '\n')
-                    if r_code[1] == "J":
-                        r_status, title, circle, cvList = match_rj(r_code)
+                    #print('Processing: ' + code)
+                    text.insert(tk.END, 'Processing: ' + code + '\n')
+                    r_status, title, circle, cvList, authorList = match_code(code)
                     # 如果顺利爬取网页信息
                     if r_status == 200 and title and circle:
                         if var1.get():
                             # 删除title中的【.*?】
                             title = re.sub(u"\\【.*?】", "", title)
 
-                        new_name = template.replace("workno", r_code)
+                        if code[0] == "R" or code[0] == "r":
+                            new_name = template_RJ.replace("workno", code)
+                        if code[0] == "B" or code[0] == "b":
+                            new_name = template_BJ.replace("workno", code)
+                        if code[0] == "V" or code[0] == "v":
+                            new_name = template_VJ.replace("workno", code)
+
                         new_name = new_name.replace("title", title)
                         new_name = new_name.replace("circle", circle)
+
+                        author = ""
+                        if authorList:  # 如果authorList非空
+                            for name in authorList:
+                                author += " " + name
+                            new_name = new_name.replace("author", author[1:])
+                        else:
+                            new_name = new_name.replace("(author)", "")  
 
                         cv = ""
                         if cvList:  # 如果cvList非空
                             for name in cvList:
-                                cv += " " + name
+                                cv += "," + name
                             new_name = new_name.replace("cv", cv[1:])
                         else:
                             new_name = new_name.replace("(cv)", "")
-
-                        # 自定义替换规则
-                        for rule in replace_rules:
-                            if 'from' in rule and 'to' in rule:
-                                new_name = new_name.replace(
-                                    rule["from"], rule["to"])
 
                         # 将Windows文件名中的非法字符替换
                         # re.sub(pattern, repl, string)
@@ -188,7 +193,7 @@ def nameChange():
                     elif r_status != "":
                         text.insert(tk.END, "**爬取DLsite过程中出现错误!\n")
                         text.insert(tk.END, "  网页 URL: " +
-                                    RJ_WEBPATH + r_code + "\n")
+                                    RJ_WEBPATH + code + "\n")
                         text.insert(tk.END, "  HTTP 响应代码: " +
                                     str(r_status) + "\n")
 
@@ -229,18 +234,27 @@ try:
     fname = os.path.join(basedir, 'config.json')
     with open(fname, 'r', encoding='utf-8') as f:
         config = json.load(f)
-        if config["template"]:  # 模板非空
-            if ("workno" in config["template"]):
-                template = config["template"]
-                text.insert(tk.END, "**使用自定义命名模板:\n")
-                text.insert(tk.END, "  " + template.strip() + "\n\n")
+        for tag in config['replace_rules']:  # 模板非空
+            if ("workno" in tag['to']):
+                if tag['type'] == "rj":
+                    text.insert(tk.END, "**使用自定义RJ命名模板:\n")
+                    template_RJ = tag['to']
+                    text.insert(tk.END, "  " + template_RJ.strip() + "\n\n")
+                if tag['type'] == "bj":
+                    text.insert(tk.END, "**使用自定义BJ命名模板:\n")
+                    template_BJ = tag['to']
+                    text.insert(tk.END, "  " + template_BJ.strip() + "\n\n")
+                if tag['type'] == "vj":
+                    text.insert(tk.END, "**使用自定义VJ命名模板:\n")
+                    template_VJ = tag['to']
+                    text.insert(tk.END, "  " + template_VJ.strip() + "\n\n")
             else:
                 text.insert(tk.END, "**模板格式错误: 模板中必须包含\"workno\"!\n")
                 text.insert(tk.END, "  使用默认命名模板:\n")
-                text.insert(tk.END, "  workno [circle] title (cv)\n\n")
+                text.insert(tk.END, "  workno title \n\n")
         else:
             text.insert(tk.END, "**使用默认命名模板:\n")
-            text.insert(tk.END, "  workno [circle] title (cv)\n\n")
+            text.insert(tk.END, "  workno title \n\n")
 
         if config["replace_rules"] and type(config["replace_rules"]) == list and len(config["replace_rules"]):
             replace_rules = config["replace_rules"]
@@ -251,7 +265,7 @@ except os.error as err:
         json.dump({'template': '', "replace_rules": []}, f, ensure_ascii=False, sort_keys=True,
                   indent=4, separators=(',', ': '))
     text.insert(tk.END, "**使用默认命名模板:\n")
-    text.insert(tk.END, "  workno [circle] title (cv)\n")
+    text.insert(tk.END, "  workno [circle] \n")
 
 var1 = tk.IntVar()  # 定义var1整型变量用来存放选择行为返回值
 cbtn = tk.Checkbutton(root, text='去除title中【】之间的内容', variable=var1,
